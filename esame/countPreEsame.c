bfc1964d367cf5581e013308deb4c4d167a35d59
runzac <91915526+alerunza@users.noreply.github.com>
2025-07-13 10:04:12 +0200

    fix count

--- esame/countPreEsame.c
+++ esame/countPreEsame.c
@@ -6,6 +6,6 @@ int count(CharTree tree, int m, int n) {
     if(m <= 0 && n >= 0){
         return tree->data + count(tree->left, m - 1, n - 1) + count(tree->right, m - 1, n - 1);
     } else{
-        return count(tree - left, m - 1, n - 1) + count(tree - right, m - 1, n - 1);
+        return count(tree -> left, m - 1, n - 1) + count(tree -> right, m - 1, n - 1);
     }
 }
